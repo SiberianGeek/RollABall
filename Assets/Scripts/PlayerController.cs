7cfc3b15cd5c02409a92b65d32391eb5cd7c60a5
FellowDeveloper <anton.tugolukov@gmail.com>
2018-07-06 10:02:57 -0700

    Followed a tutorial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            count++;
+            SetCountText();
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Roll a Ball tutorial
+
+Result of following Unity's Roll a Ball tutorial.
+
+## Reflections
+
+* Everything on the scene is a game object.
+* Triggering collisions is possible by checking "is trigger" on object's Rigidbody material (PickUp boxes)
+* Normal rigidbodies are controlled by forces.
+* Rigidbodies with "is kinematic" checked are controlled by their **transform** component 
